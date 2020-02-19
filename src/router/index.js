3e9208f59e40b2c3e3dab64d19cfe22383495024
曾可爱 <123@.com>
2020-02-19 15:04:55 +0800

    路由创建

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+
+// 0. 导入 子组件
+// import son from './components/son.vue'
+// 1. 导入 vue-router
+import VueRouter from 'vue-router'
+// 2. 注册 vue-router
+Vue.use(VueRouter)
+// 3. 实例化
+const router = new VueRouter({
+//这里就是路由的配制项
+routes: [
+//  {
+// path: '/son',//配置地址
+// component: son  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
+//  }
+  ]
+}) // 4. 需要在下面注入router
+
+export default router
