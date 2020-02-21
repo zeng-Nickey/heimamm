949c307459749033c0ebbe44525cb1fe01d56aa6
曾可爱 <123@.com>
2020-02-21 18:49:55 +0800

    新建reg分支

---
+++ src/views/login/components/reg.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="活动名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        dialogFormVisible: false,
+        formLabelWidth:"65px",
+        form: {
+          name: '',
+          
+        },
+        formLabelWidth: '120px'
+      };
+        
+    },
+};
+</script>
+
+<style>
+</style>
